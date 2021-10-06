87b630c334b522d18440e2da813b1d8900eb2dfb
Vinh Tang <vinhtq@ghn.vn>
2021-10-06 17:21:09 +0700

    Update

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "78c52cdbd44eb5eb93b323c70bc2ad53",
-"/": "78c52cdbd44eb5eb93b323c70bc2ad53",
-"main.dart.js": "67e9f331e6348056fa40d2b99e5781a4",
+"index.html": "52ddad5f621c833d63bf2ce8070c2110",
+"/": "52ddad5f621c833d63bf2ce8070c2110",
+"main.dart.js": "e07b10943691013c924f08b16071b719",
 "manifest.json": "4e9b2e0dd89921f70b118f04eb04b7f4",
 "version.json": "c0cc30511d8035a9c6601db3ec9d6903"
 };
